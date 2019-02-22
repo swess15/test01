39beee88b34190342eb73682804568c820cbfd08
swess15 <swess15@gmail.com>
2019-02-22 10:17:01 +0900

    Updates

---
+++ precache-manifest.872f40949ffab035ea4c72cc34820632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test01/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9414f09f3e6e14a456cb",
+    "url": "/test01/static/js/runtime~main.9414f09f.js"
+  },
+  {
+    "revision": "8a63afa58c1ddeec6d54",
+    "url": "/test01/static/js/main.8a63afa5.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/test01/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "8a63afa58c1ddeec6d54",
+    "url": "/test01/static/css/main.8d1a5832.chunk.css"
+  },
+  {
+    "revision": "e8d18e48d4cb15f5c627925ec2a664b9",
+    "url": "/test01/index.html"
+  }
+];
